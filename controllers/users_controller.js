75385d15b2ef7adb2f73f3c397b3d6328684efff
kashishsaini <uietece1923kashishsaini@kuk.ac.in>
2023-03-22 10:27:21 +0530

    Setting up Schema, Rendering Sign up and Sign in pages and Creating Form for sign in and up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,27 @@ module.exports.profile= function(req,res){
         title:"Profile"
     })
 }
+
+// render the Sign Up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    })
+}
+
+// render the Sign In page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:'Codeial | Sign In'
+    })
+}
+
+// get the sign Up data
+module.exports.create=function(req,res){
+    //later
+}
+
+// sign in and create a session for the user
+module.exports.createSession=function(req,res){
+    // later
+}
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,12 @@ const router =express.Router();
 const userController= require('../controllers/users_controller');
 const postController= require('../controllers/post_controller');
 
+
 router.get('/profile', userController.profile);
 router.get('/posts', postController.posts);
+
+router.get('/sign-up',userController.signUp);
+router.get('/sign-in',userController.signIn);
+
 
 module.exports=router;
