417dfefaf7abc9f9fadd492458d09124ee0b7884
kashishsaini <uietece1923kashishsaini@kuk.ac.in>
2023-03-25 14:41:56 +0530

    Authentication passport js setup

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,26 @@
 const express =require('express');
-
 const router =express.Router();
+const passport=require('passport');
+
 const userController= require('../controllers/users_controller');
 const postController= require('../controllers/post_controller');
 
 
-router.get('/profile', userController.profile);
+router.get('/profile',passport.checkAuthentication, userController.profile);
 router.get('/posts', postController.posts);
 
 router.get('/sign-up',userController.signUp);
 router.get('/sign-in',userController.signIn);
+//router.get('/delete',userController.delete);
 
 router.post('/create',userController.create);
-router.post('/create-session',userController.createSession);
+//router.post('/create-session',userController.createSession);
+
+// use passport as a middleware
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+) , userController.createSession);
 
 
 module.exports=router;
